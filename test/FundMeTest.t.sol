1a34415654b1aba5bd5620a53664ec2d4eafe76f
Ab-hai <chandrakarabhay590@gmail.com>
2026-02-06 17:36:49 +0000

    Added a script file

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+import {Test , console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+    function setUp() external {
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+    }
+
+    function testDemo() public {
+    assertEq(fundMe.MINIMUM_USD(),5 * 10 ** 18);
+    }
+
+    function testOwner() public {
+        assertEq(fundMe.i_owner(),msg.sender);
+    }
+
+      function testPriceFeedVersionIsAccurate() public {
+        if (block.chainid == 11155111) {
+            uint256 version = fundMe.getVersion();
+            assertEq(version, 4);
+        } else if (block.chainid == 1) {
+            uint256 version = fundMe.getVersion();
+            assertEq(version, 6);
+        }
+  }       
+}
