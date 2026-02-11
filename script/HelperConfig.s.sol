79602bf457c85876ea46b3290aa82b953505f63f
Ab-hai <chandrakarabhay590@gmail.com>
2026-02-11 18:18:18 +0000

    fix:formatted contracts

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -5,45 +5,37 @@ import {Script} from "forge-std/Script.sol";
 import {MockV3Aggregator} from "../test/mocks/Mock.sol";
 
 contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
 
-NetworkConfig public activeNetworkConfig;
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
 
-
-uint8 public constant DECIMALS = 8;
-int256 public constant INITIAL_PRICE = 2000e8;
- 
- struct NetworkConfig {
-    address priceFeed;
- }
-
- constructor() {
-    if (block.chainid == 11155111) {
-        activeNetworkConfig = getSepoliaEth();
-    } else {
-        activeNetworkConfig = getAnvilEth();
+    struct NetworkConfig {
+        address priceFeed;
     }
- }
 
- function getSepoliaEth() public pure returns (NetworkConfig memory) {
-    NetworkConfig memory sepoliaConfig = NetworkConfig({
-        priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-    });
-    return sepoliaConfig;
- }
- 
- function getAnvilEth() public returns (NetworkConfig memory) {
-    if (activeNetworkConfig.priceFeed != address(0)) {
-        return activeNetworkConfig;
-        
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEth();
+        } else {
+            activeNetworkConfig = getAnvilEth();
+        }
     }
-    vm.startBroadcast();
-    MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
-    vm.stopBroadcast();
 
-    NetworkConfig memory anvilConfig = NetworkConfig({
-        priceFeed: address(mockPriceFeed)
-    });
-    return anvilConfig;
- }
+    function getSepoliaEth() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
+        return sepoliaConfig;
+    }
 
+    function getAnvilEth() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
+        return anvilConfig;
+    }
 }
--- src/FundMe.sol
+++ src/FundMe.sol
@@ -56,11 +56,7 @@ contract FundMe {
         fund();
     }
 
-
-    function getAddressToAmountFunded(
-        address fundingAddress
-    )
-    external view returns (uint256){
+    function getAddressToAmountFunded(address fundingAddress) external view returns (uint256) {
         return s_addressToAmountFunded[fundingAddress];
     }
 
